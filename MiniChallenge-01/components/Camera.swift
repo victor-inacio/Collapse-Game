1bec1069f728b4d77b9a5bcdfcd10192f0eee6ea
Victor Inácio <chustrupgamer@gmail.com>
2023-07-13 16:22:47 -0300

    CameraController code refactored

--- MiniChallenge-01/components/Camera.swift
+++ MiniChallenge-01/components/Camera.swift
@@ -23,34 +23,21 @@ class CameraController {
         }
     }
     
+    func onFinishUpdate() {
+        if let target = target, let boundaries = boundaries {
+            self.camera.run(.move(to: target.position, duration: 0.3))
+        }
+    }
+    
     private func createConstraints() {
-        let boundX1 = self.boundaries!.frame.minX
-        let boundX2 = self.boundaries!.frame.maxX
-        let boundY1 = self.boundaries!.frame.maxY
-        let boundY2 = self.boundaries!.frame.minY
+    
+        let boundLeft = self.boundaries!.calculateAccumulatedFrame().minX + self.camera.scene!.size.width / 2
+        let boundRight = self.boundaries!.calculateAccumulatedFrame().maxX - self.camera.scene!.size.width / 2
         
-        let cameraCamX1 = self.camera.scene!.frame.minX
-        let cameraCamY1 = self.camera.scene!.frame.minY
+        let boundariesConstraint = SKConstraint.positionX(SKRange(lowerLimit: boundLeft, upperLimit: boundRight))
         
-        let distanceFromCamCenterToCamBoundsHorizonally = self.camera.position.x - cameraCamX1
-        let distanceFromCamCenterToCamBoundsVertically = self.camera.position.y - cameraCamY1
-        
-        let maxCameraX1 = boundX1 + distanceFromCamCenterToCamBoundsHorizonally
-        let maxCameraX2 = boundX2 - distanceFromCamCenterToCamBoundsHorizonally
-        let maxCameraY1 = boundY1 - distanceFromCamCenterToCamBoundsVertically
-        let maxCameraY2 = boundY2 + distanceFromCamCenterToCamBoundsVertically
-        
-        let horizontalCamRange = SKRange(lowerLimit: maxCameraX1, upperLimit: maxCameraX2)
-        let verticalCamRange = SKRange(lowerLimit: maxCameraY2, upperLimit: maxCameraY1)
-        let distanceFromPlayerRange = SKRange(constantValue: 0)
-
-        let horizontalBorderConstraint = SKConstraint.positionX(horizontalCamRange)
-        let verticalBorderConstraint = SKConstraint.positionY(verticalCamRange)
-        let distanceFromPlayerConstraint = SKConstraint.distance(distanceFromPlayerRange, to: self.target!)
         self.camera.constraints = [
-            distanceFromPlayerConstraint,
-            horizontalBorderConstraint,
-            verticalBorderConstraint
+            boundariesConstraint
         ]
     }
 }
