053ce3b559c5f55545b79f89a759c0511e0d54c2
Victor Inácio <chustrupgamer@gmail.com>
2023-07-18 18:07:59 -0300

    Door Component transition direction now relative to boundaries

--- MiniChallenge-01/components/DoorComponent.swift
+++ MiniChallenge-01/components/DoorComponent.swift
@@ -11,10 +11,10 @@ class DoorComponent: GKComponent {
                 let levelName = userData.value(forKey: "levelName") as! String
                 
                 if let level = SKScene(fileNamed: levelName) {
-                    let transition = SKTransition.push(with: .left, duration: 0.5)
                     
-
-                    
+                    let direction = self.getTransitionDirection()
+                    let transition = SKTransition.push(with: direction, duration: 0.5)
+                   
                     node.scene!.view?.presentScene(level, transition: transition)
                 }
                
@@ -23,5 +23,27 @@ class DoorComponent: GKComponent {
         }
 
         self.entity?.addComponent(triggerComponent)
+    }
+    
+    private func getTransitionDirection() -> SKTransitionDirection {
+        
+        var transition = SKTransitionDirection.left
+        
+        let node = self.entity!.component(ofType: SpriteComponent.self)!.node!
+        
+        let nodeScene = node.scene! as! BaseLevelScene
+        
+        if let sceneBoundaries = nodeScene.getBoundaries() {
+            
+            print(node.position.x, sceneBoundaries.position.x)
+            
+            if (node.position.x < sceneBoundaries.position.x) {
+                transition = .right
+            }
+            
+        }
+        
+        return transition
+        
     }
 }
