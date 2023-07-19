3568ade792e3e2285b98e5b0684f8b8e4e9ae65c
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-07-18 21:07:08 -0300

    states

--- MiniChallenge-01/components/Player.swift
+++ MiniChallenge-01/components/Player.swift
@@ -11,16 +11,15 @@ import GameplayKit
 class Player: VirtualControllerTarget{
     
     var playerNode: SKSpriteNode!
-    var stateMachine: GKStateMachine?
+    var stateMachine: GKStateMachine!
     var velocityX: CGFloat = 0
     var angle: CGFloat = 0
     
     init(){
+        
         let texture = SKTexture(imageNamed: "player")
         
         playerNode = SKSpriteNode(texture: texture, color: .red, size: texture.size())
-        
-        playerNode.name = "player"
         
         playerNode.zPosition = 1
         
@@ -28,6 +27,7 @@ class Player: VirtualControllerTarget{
         playerNode.physicsBody?.collisionBitMask = PhysicsCategory.player.rawValue
         playerNode.physicsBody?.allowsRotation = false
         playerNode.physicsBody?.isDynamic = true
+        
     }
     
     func applyMachine(){
@@ -40,149 +40,141 @@ class Player: VirtualControllerTarget{
     }
     
     func onJoystickChange(direction: CGPoint, angle: CGFloat) {
-    
-        applyMovement(distanceX: direction.x, angle: angle)
-    
+        
+        if stateMachine.currentState is PlayerDash == false{
+            applyMovement(distanceX: direction.x, angle: angle)
+        }
         velocityX = direction.x
         self.angle = angle
         
-        let xDirection: CGFloat = direction.x < 0 ? -1 : 1
-        playerNode.xScale = xDirection
+        if direction.x != 0 && stateMachine.currentState is PlayerDash == false{
+            stateMachine?.enter(PlayerRun.self)
+        }
         
     }
-
-    func onJoystickJumpBtnTouch() {
-        jump()
-    }
     
-    func onJoystickDashBtnTouch(direction: CGVector) {
-        dash(direction: direction)
+    func onJoystickJumpBtnTouch() {
+        
+        stateMachine?.enter(PlayerJump.self)
+        
+        if stateMachine.currentState is PlayerDash == false{
+            jump()
+        }
+        
     }
     
     func applyMovement(distanceX: CGFloat, angle: CGFloat){
         
-        playerNode.physicsBody!.velocity.dx = distanceX  * 4
-        
-            
-        if angle > 1.51 || angle < -1.51{
-
-            playerNode.xScale = -1
-
-        } else{
-            
-            playerNode.xScale = 1
-            
+        if stateMachine.currentState is PlayerDash == false{
+            playerNode.physicsBody!.velocity.dx = distanceX  * 4
         }
         
-        
-        if distanceX != 0{
-            
-            stateMachine?.enter(PlayerRun.self)
+        if angle > 1.51 || angle < -1.51{
+            playerNode.xScale = -1
+        } else{
+            playerNode.xScale = 1
         }
     }
     
     func update() {
-        applyMovement(distanceX: velocityX, angle: angle)
+        if stateMachine.currentState is PlayerDash == false{
+            applyMovement(distanceX: velocityX, angle: angle)
+        }
     }
     
     func jump(){
         
-<<<<<<< Updated upstream
-        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0 , dy: playerNode.size.height / 2))
-=======
-       
-    playerNode.physicsBody?.applyImpulse(CGVector(dx: 0 , dy: playerNode.size.height / 2))
-        
+        if stateMachine.currentState is PlayerDash == false{
+            playerNode.physicsBody?.applyImpulse(CGVector(dx: 0 , dy: playerNode.size.height / 2))
+        }
     }
     
     func onJoystickDashBtnTouch(direction: CGVector) {
->>>>>>> Stashed changes
         
-        stateMachine?.enter(PlayerJump.self)
-    }
-    //
-    func dash(direction: CGVector){
-        
-        playerNode.physicsBody?.applyImpulse(CGVector(dx: direction.dx * 50 , dy: direction.dy * 50 ))
+        dash(direction: direction)
         
         stateMachine?.enter(PlayerDash.self)
         
-<<<<<<< Updated upstream
-//        print(direction)
-=======
     }
     
     func dash(direction: CGVector){
         
         if stateMachine.currentState is PlayerDash == false{
-            playerNode.physicsBody?.applyImpulse(CGVector(dx: direction.dx * 30 , dy: direction.dy * 30 ))
+            playerNode.physicsBody?.applyImpulse(CGVector(dx: direction.dx * 35 , dy: direction.dy * 35 ))
         }
  
         self.playerNode.run(.sequence([.wait(forDuration: 0.5), .run{
             self.stateMachine?.enter(PlayerIdle.self)
         }]))
->>>>>>> Stashed changes
     }
+    
 }
 
 class PlayerIdle: GKState {
     
+    var player = Player()
+    
+    
     override func didEnter(from previousState: GKState?) {
         
-   
-    }
-    
-    func move() {
-        
+        print("idle")
     }
 }
 
 class PlayerRun: GKState{
     
-<<<<<<< Updated upstream
-    //    var gameScene = PlataformGameScene()
     var isRunning = false
-    
-    override func didEnter(from previousState: GKState?) {
-        
-//        print("Estou correndo")
-        
-        isRunning = true
-=======
-
     var player = Player()
     
     
     override func didEnter(from previousState: GKState?) {
         
         print("run")
-
->>>>>>> Stashed changes
+        isRunning = true
+        
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        
+        
         
     }
 }
 
 class PlayerJump: GKState{
     
-<<<<<<< Updated upstream
     var player = Player()
-    var onAir = false
     
-=======
->>>>>>> Stashed changes
+    
+    
     override func didEnter(from previousState: GKState?) {
-
-        onAir = true
+        
+        print("jump")
+        
     }
 }
 
 class PlayerDash: GKState{
     
     override func didEnter(from previousState: GKState?) {
+        print("dash")
+    }
+    
+    override func isValidNextState(_ stateClass: AnyClass) -> Bool {
         
+        switch stateClass{
+            
+        case is PlayerRun:
+            
+            return false
+            
+        default:
+            
+            return true
+        }
     }
     
     override func willExit(to nextState: GKState) {
-       
+        
     }
 }
