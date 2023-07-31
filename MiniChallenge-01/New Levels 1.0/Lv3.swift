1dd58f8195aec8ef5e31469c789cd21451879ab9
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-07-31 20:17:32 -0300

    Mais uma atualização

--- MiniChallenge-01/New Levels 1.0/Lv3.swift
+++ MiniChallenge-01/New Levels 1.0/Lv3.swift
@@ -16,8 +16,9 @@ class Lv3:BaseLevelScene{
         super.didMove(to: view)
         
         parallax = Parallax(scene: self, items: [
-            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 200, dy: 500)),
-            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 200, dy: 1500)),
+            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 500, dy: 500)),
+            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 500, dy: 1500)),
+            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 500, dy: 2400)),
             .init(fileName: "New planet 2", velocityFactor: 0.012, zIndex: -4, offset: CGVector(dx: 0, dy: 350)),
             .init(fileName: "New Planet 3", velocityFactor: 0.10, zIndex: -3, offset: CGVector(dx: -140, dy: 350)),
         ])
