462879836429ef24fd3fb418881a8cedb67f9921
Leonardo Mesquita Alves <leonardo.pro.alves@gmail.com>
2023-07-31 18:28:33 -0300

    .

--- MiniChallenge-01/New Levels 1.0/Lv1.swift
+++ MiniChallenge-01/New Levels 1.0/Lv1.swift
@@ -10,6 +10,15 @@ import SpriteKit
 import GameplayKit
 
 class Lv1: BaseLevelScene{
-    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        parallax = Parallax(scene: self, items: [
+            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 200, dy: 300)),
+            .init(fileName: "New Planet", velocityFactor: 0, zIndex: -5, offset: CGVector(dx: 1700, dy: 300)),
+            .init(fileName: "New planet 2", velocityFactor: 0.012, zIndex: -4, offset: CGVector(dx: 0, dy: 150)),
+            .init(fileName: "New Planet 3", velocityFactor: 0.10, zIndex: -3, offset: CGVector(dx: -140, dy: 150)),
+        ])
+    }
 }
 
