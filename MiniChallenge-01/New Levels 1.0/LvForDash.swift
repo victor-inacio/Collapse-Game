63de490a575e5d6828e562f55ae784f5ce526b60
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-08-01 11:12:33 -0300

    .

---
+++ MiniChallenge-01/New Levels 1.0/LvForDash.swift
@@ -0,0 +1,14 @@
+//
+//  lvForDash.swift
+//  MiniChallenge
+//
+//  Created by Gabriel Eirado on 01/08/23.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class LvForDash: BaseLevelScene{
+    
+}
